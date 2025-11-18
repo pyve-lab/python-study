a3b7f378391617977cb307634c23216c8be332f0
mmi-ing <tpgns252@gmail.com>
2025-11-18 23:33:17 +0900

    09 Strings

---
+++ Python_Tutorial/09_Strings_Exercises.py
@@ -0,0 +1,128 @@
+####################################################################################
+# 파이썬 - 문자열 연습
+####################################################################################
+# Correct Answer!
+# QQQQQQQQQQQQQQQQQQ
+# Exercise: Python Strings
+# QQQQQQQQQQQQQQQQQQ
+# What will be the result of the following code:
+x = 'Welcome'
+print(x[3])
+# Wel
+# c                 # Correct Answer!
+# Welcome Welcome Welcome
+
+# Use the len function to print the length of the string.
+# x = "Hello World"
+# print(?)
+# print(len(x))     # Correct Answer!
+
+# Get the first character of the string txt.
+# txt = "Hello World"
+# x = ?
+# txt[0]            # Correct Answer!
+
+# Insert the correct keyword to check if the word 'free' is present in the text:
+# txt = 'The best things in life are free!'
+# if 'free' ? txt:
+#   print('Yes, free is present in the text.')
+# in                # Correct Answer!
+
+# QQQQQQQQQQQQQQQQQQ
+# Exercise: Python Slicing Strings
+# QQQQQQQQQQQQQQQQQQ
+# What will be the result of the following code:
+# x = 'Welcome'
+# print(x[3:5])
+# lcome
+# come
+# com
+# co                # Correct Answer!
+
+# Get the characters from index 2 to index 4 (llo).
+# txt = "Hello World"
+# x = ?
+# txt[2:5]          # Correct Answer!
+
+# What will be the result of the following code:
+# x = 'Welcome'
+# print(x[3:])
+# lcome
+# come              # Correct Answer!
+# com
+# co
+
+# QQQQQQQQQQQQQQQQQQ
+# Exercise: Python Modify Strings
+# QQQQQQQQQQQQQQQQQQ
+# What is a correct syntax to print a string in upper case letters?
+# 'Welcome'.upper() # Correct Answer!
+# 'Welcome'.toUpper()
+# 'Welcome'.toUpperCase()
+
+# Return the string without any whitespace at the beginning or the end.
+# txt = " Hello World "
+# x =  ?
+# txt.strip()       # Correct Answer!
+
+# Convert the value of txt to upper case.
+# txt = "Hello World"
+# txt = ?
+# txt.upper()       # Correct Answer!
+
+# Convert the value of txt to lower case.
+# txt = "Hello World"
+# txt = ?
+# txt.lower()       # Correct Answer!
+
+# Replace the character H with a J.
+# txt = "Hello World"
+# txt = txt.(?, ?)
+# txt = txt.replace("H", "J")       # Correct Answer!
+
+# QQQQQQQQQQQQQQQQQQ
+# Exercise: Python Concatenate Strings
+# QQQQQQQQQQQQQQQQQQ
+# What is a correct syntax to merge variable x and y into variable z?
+# z = x, y
+# z = x = y
+# z = x + y             # Correct Answer!
+
+# What will be the result of the following code:
+# x = 'Welcome'
+# y = 'Coders'
+# print(x + y)
+# Welcome Coders
+# WelcomeCoders         # Correct Answer!
+# Welcome
+# Coders
+
+# Consider this code:
+# a = 'Join'
+# b = 'the'
+# c = 'party'
+# print(a + b + c)
+# print(a + ' ' + b + ' ' + c)      # Correct Answer!
+# print(a b c)
+
+# QQQQQQQQQQQQQQQQQQ
+# Exercise: Python Format Strings
+# QQQQQQQQQQQQQQQQQQ
+# If x = 9, what is a correct syntax to print 'The price is 9.00 dollars'?
+# print(f'The price is {x:.2f} dollars')        # Correct Answer!
+# print(f'The price is {x:2} dollars')
+# print(f'The price is {x:format(2)} dollars')
+
+# Insert the correct syntax to add a placeholder for the age parameter.
+# age = 36
+# txt = f"My name is John, and I am "?"
+# print(txt)
+# {age}         # Correct Answer!
+
+# What will be the result of the following code:
+# print(f'The price is {2 + 3} dollars')
+# The price is 23 dollars
+# The price is 5 dollars        # Correct Answer!
+# The price is {2 + 3} dollars
+# The price is 6 dollars
+
