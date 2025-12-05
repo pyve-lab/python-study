0286443c0baf7b5c8012d2b90192eae72796a120
jjoyh0123 <jjoyh0123@gmail.com>
2025-12-06 00:41:58 +0900

    feat: add modules_external_features

---
+++ python_tutorial/14_modules_external_features/mymodule.py
@@ -0,0 +1,14 @@
+
+# 01_python_modules.ipynb 에서 mymodule를 import 해보기 위해 만든 파일입니다.
+
+# 이 코드를 다음 이름의 파일에 저장하세요.mymodule.py
+def greeting(name):
+  print("Hello, " + name)
+  
+  
+  
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
